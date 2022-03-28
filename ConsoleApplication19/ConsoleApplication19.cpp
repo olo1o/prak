1ca02cfb116968ff8523d7e75033aa0970efbab1
123 <aztttty@gmail.com>
2022-03-28 12:34:33 +0300

    1

--- ConsoleApplication19/ConsoleApplication19.cpp
+++ ConsoleApplication19/ConsoleApplication19.cpp
@@ -24,22 +24,24 @@ void print_array2(int m[], int size)
 }
 void print_array3(int m[], int size)
 {
-	int sum = 0;
-	int total = 0;
+	double sum = 0;
+	double total = 0;
 	for (int i = 0; i < size; i++)
 	{
 		sum += m[i];
 		total++;
 	}
-	cout << sum / total;
-	
+	cout << sum / total << "\n";
 }
+
 int main()
 {
 	int ar[5] = {};
+	
 	// пример передачи массива в качестве параметра
 	print_array(ar, 5); // первый параметр - имя массива (без квадратных скобок)
 	// второй параметр - какое количество элементов необходимо обработать
 	print_array2(ar, 5);
 	print_array3(ar, 5);
+	cout << ar[0];
 }
